0c0ed17456630fb6ab26177b998b05c7b1fd59bf
Full Aware <fullaware@users.noreply.github.com>
2025-04-04 11:20:01 -0400

    update build

--- build.sh
+++ build.sh
@@ -1,6 +1,2 @@
 docker build . -t fullaware/beryl:latest
-<<<<<<< HEAD
 docker push fullaware/beryl:latest
-=======
-docker push fullaware/beryl:latest
->>>>>>> 3b6f76ba0a387b2bc8be692eb927bf36677133c1
